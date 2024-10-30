5f83050867bd8295886330f3f510db4e1f4af3eb
seankennelly <sean.kennelly@hotmail.co.uk>
2024-10-30 10:57:33 +0000

    Update layout blade so that favicon relative links use url() helper

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,9 +5,9 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         {{-- Favicons --}}
-        <link rel="apple-touch-icon" sizes="180x180" href="images/favicons/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="images/favicons/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="images/favicons/favicon-16x16.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ url ('images/favicons/apple-touch-icon.png') }}">
+        <link rel="icon" type="image/png" sizes="32x32" href="{{ url ('images/favicons/favicon-32x32.png') }}">
+        <link rel="icon" type="image/png" sizes="16x16" href="{{ url ('images/favicons/favicon-16x16.png') }}">
         {{-- Alpine.js --}}
         <script src="//unpkg.com/alpinejs" defer></script>
         {{-- Stylesheets --}}
