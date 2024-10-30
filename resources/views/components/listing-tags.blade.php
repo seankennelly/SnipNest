5ce11cbd941ffdc39ec40ac000f07a880762c1f2
seankennelly <sean.kennelly@hotmail.co.uk>
2024-10-30 10:29:19 +0000

    Update anchor in listing tag as filter url was not correctly implemented in last commit

--- resources/views/components/listing-tags.blade.php
+++ resources/views/components/listing-tags.blade.php
@@ -11,7 +11,7 @@
     'flex items-center justify-center',
     'bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs'
     ]) }}>
-    <a href="{{ url('/', ['tag' => $tag]) }}">{{$tag}}</a>
+    <a href="{{ url('/') }}?tag={{ urlencode($tag) }}">{{$tag}}</a>
   </li>
   @endforeach
 </ul>
